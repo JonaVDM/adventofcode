d13a5a7e7dc6ec816b484971e42f57f26efa426e
Jonathan <jonajvdm@gmail.com>
2020-12-01 12:00:30 +0100

    Improved performance a bit on 2020day1

--- day1/index.ts
+++ day1/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import { cpuUsage } from 'process';
+import { performance } from 'perf_hooks';
 
 function getInput() {
     const file = fs.readFileSync(path.join(__dirname, 'input'));
@@ -8,34 +8,38 @@ function getInput() {
 }
 
 (async () => {
+    const t0 = performance.now();
     const input = getInput().split('\n');
     let completed = [false, false];
 
-    for (let a of input) {
-        let num1 = Number(a);
+    for (let i = 0; i < input.length; i++) {
+        let num1 = Number(input[i]);
 
-        for (let b of input) {
-            let num2 = Number(b);
+        for (let j = i + 1; j < input.length; j++) {
+            let num2 = Number(input[j]);
 
-                if (!completed[0] && num1 + num2 == 2020) {
-                    console.log(`Match for part 1 ${num1}x${num2}=`);
-                    console.log(num1 * num2);
-                    completed[0] = true;
-                }
+            if (!completed[0] && num1 + num2 == 2020) {
+                console.log(`Match for part 1 ${num1}x${num2}=`);
+                console.log(num1 * num2);
+                completed[0] = true;
+            }
 
-            for (let c of input) {
-                let num3 = Number(c);
+            for (let k = j + 1; k < input.length; k++) {
+                let num3 = Number(input[k]);
 
                 if (!completed[1] && num1 + num2 + num3 == 2020) {
                     completed[1] = true;
                     console.log(`Match for part 2 ${num1}x${num2}x${num3}=`);
                     console.log(num1 * num2 * num3);
-                }
-
-                if (completed[0] && completed[1]) {
                     break;
                 }
+
             }
         }
+        if (completed[0] && completed[1]) {
+            break;
+        }
     }
+
+    console.log(`Solved? in ${performance.now() - t0}ms`);
 })();
