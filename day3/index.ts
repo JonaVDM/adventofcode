8196c688c76d171346d2e968216350b66a8f30de
Jonathan <jonajvdm@gmail.com>
2020-12-03 07:36:49 +0100

    Completed day 3 part 2

--- day3/index.ts
+++ day3/index.ts
@@ -7,30 +7,54 @@ function getInput() {
     return file.toString().trim();
 }
 
+class MapSolver {
+    private input: string[]
+    private lineLength: number;
+
+    constructor(input: string[]) {
+        this.input = input;
+        this.lineLength = input[0].length;
+    }
+
+    getSlope(right: number, down: number) {
+        let x = 0;
+        let y = 0;
+
+        let trees = 0;
+
+        while (y < this.input.length - 1) {
+            // 3 right, 1 down
+            x += right;
+            y += down;
+
+            if (x >= this.lineLength) {
+                x -= this.lineLength;
+            }
+
+            if (this.input[y][x] == '#') {
+                trees++;
+
+            }
+        }
+
+        return trees;
+    }
+}
+
 (async () => {
     const t0 = performance.now();
     const input = getInput().split('\n');
-    const endLine = input.length - 1;
-    let currentLine = 0;
-    let currentpos = 0;
-    const linelength = input[0].length;
 
-    let trees = 0;
+    const mapSolver = new MapSolver(input);
 
-    while (currentLine < endLine) {
-        // 3 right, 1 down
-        currentLine += 1;
-        currentpos += 3;
+    let total = 1;
+    let inputs: [number, number][] = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
 
-        if (currentpos >= linelength) {
-            currentpos -= linelength;
-        }
+    inputs.forEach(item => {
+        total *= mapSolver.getSlope(...item);
+    });
 
-        if (input[currentLine][currentpos] == '#') {
-            trees++;
-        }
-    }
-
-    console.log(trees);
+    console.log(`Part 1 = ${mapSolver.getSlope(3, 1)}`);
+    console.log(`Part 2 = ${total}`);
     console.log(`Solved? in ${performance.now() - t0}ms`);
 })();
